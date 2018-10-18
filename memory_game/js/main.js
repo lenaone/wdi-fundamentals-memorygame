6ba1626a4b912cec39d87e3eebbdb5505d3a4466
Lena Seol <lena@fabios-mbp.home>
2018-10-18 17:31:37 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,13 @@
 cards = ["queen", "queen", "king", "king"];
 cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+var checkForMatch = function () {
+
 }
-else {
-	alert("sorry, try again");
-}
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
