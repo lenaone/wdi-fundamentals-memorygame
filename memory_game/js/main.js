01bd3e074b0dd7181f6e2529badb3a5f59897c9b
Lena Seol <lena@fabios-mbp.home>
2018-10-19 18:32:02 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
-cards = ["queen", "queen", "king", "king"];
+var cards = [
+{ rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png" },
+
+{rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png" },
+
+{rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png" },
+
+{rank: "king",
+suit: "diamons",
+cardImage: "images/king-of-diamonds.png" }
+];
 cardsInPlay = [];
 var checkForMatch = function () {
 
 }
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 };
 
